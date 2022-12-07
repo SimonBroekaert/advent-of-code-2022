1b51141233c736becf38cf81676819c4eae2bf69
Simon Broekaert <simon_broekaert@hotmail.com>
2022-12-07 09:25:25 +0100

    WIP day 7

--- app/Http/Controllers/DaySevenController.php
+++ app/Http/Controllers/DaySevenController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Storage;
 
 class DaySevenController extends Controller
 {
     public function __invoke(): View
     {
-        $question1 = null;
-        $answer1 = null;
+        $structure = $this->getDirStructure();
+
+        $question1 = 'Sum of all directory sizes of directories smaller than 100000';
+        $answer1 = $this->getSizeOfDirectoriesSmallerThan($structure, 100000);
 
         $question2 = null;
         $answer2 = null;
@@ -20,5 +25,90 @@ class DaySevenController extends Controller
             'question2',
             'answer2'
         ));
+    }
+
+    private function getDirStructure(): Collection
+    {
+        $commands = $this->getCommands();
+
+        $structure = [];
+        $currentDir = '';
+
+        foreach ($commands as $command) {
+            if ($command->type === 'cd') {
+                $dir = $command->arguments[0];
+                $currentDir = match ($dir) {
+                    '..' => Str::beforeLast($currentDir, '.'),
+                    '/' => '',
+                    default => $currentDir === '' ? $dir : $currentDir . '.' . $dir,
+                };
+            }
+            if ($command->type === 'ls') {
+                $files = collect($command->output)->where('type', 'file')->toArray();
+                data_set($structure, $currentDir === '' ? '_files' : $currentDir . '._files', $files);
+            }
+        }
+
+        $structure = $this->calculateDirSizesRecursive(collect($structure));
+
+        return collect($structure);
+    }
+
+    private function calculateDirSizesRecursive(Collection $structure): Collection
+    {
+        return $structure->map(function ($content, $dir) {
+            $size = 0;
+
+            $calculatedContent = [];
+            if ($dir === '_files') {
+                $size += collect($content)->sum('size');
+            } else {
+                $calculatedContent = $this->calculateDirSizesRecursive(collect($content));
+            }
+
+            $size += collect($calculatedContent)->sum('_size');
+
+            return collect([
+                ...$calculatedContent,
+                '_size' => $size,
+            ]);
+        });
+    }
+
+    private function getCommands(): Collection
+    {
+        $file = Storage::disk('public')->get('day-7.txt');
+
+        return collect(explode('$', $file))
+            ->filter(fn ($line) => trim($line) !== '')
+            ->map(function ($content) {
+                $command = trim(Str::before($content, "\n"));
+                $output = array_filter(explode("\n", trim(Str::after($content, "\n"))));
+
+                return (object) [
+                    'type' => Str::before($command, ' '),
+                    'arguments' => Str::contains($command, ' ') ? array_map('trim', explode(' ', Str::after($command, ' '))) : null,
+                    'output' => collect($output)->map(function ($outputLine) {
+                        return (object) [
+                            'type' => Str::before($outputLine, ' ') === 'dir' ? 'directory' : 'file',
+                            'name' => Str::after($outputLine, ' '),
+                            'size' => Str::before($outputLine, ' ') === 'dir' ? 0 : (int) Str::before($outputLine, ' '),
+                        ];
+                    })->toArray(),
+                ];
+            });
+    }
+
+    private function getSizeOfDirectoriesSmallerThan(Collection $structure, int $size): int
+    {
+        return $structure->filter(fn ($content, $dir) => $dir !== '_files')->sum(function ($content) use ($size) {
+            $dirSize = $content['_size'] ?? 0;
+
+            if ($dirSize < $size) {
+                return $dirSize;
+            }
+
+            return $this->getSizeOfDirectoriesSmallerThan(collect($content), $size);
+        });
     }
 }
