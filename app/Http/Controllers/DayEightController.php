90bb2ecf3c9df3d1a11ec2d3a7f66acc0967c36a
Simon Broekaert <simon_broekaert@hotmail.com>
2022-12-08 09:25:37 +0100

    WIP Day-8... Optimize to fix execution time error

--- app/Http/Controllers/DayEightController.php
+++ app/Http/Controllers/DayEightController.php
@@ -3,13 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
 
 class DayEightController extends Controller
 {
     public function __invoke(): View
     {
-        $question1 = null;
-        $answer1 = null;
+        $forest = $this->getForest();
+
+        $question1 = "Amount of trees visible from outside the grid";
+
+        $lastRow = $forest->max('row');
+        $lastColumn = $forest->max('column');
+        $answer1 = $forest->filter(function ($tree) use ($forest, $lastRow, $lastColumn) {
+            if ($tree->row === 0 || $tree->row === $lastRow || $tree->column === 0 || $tree->column === $lastColumn) {
+                return true;
+            }
+            $visibleFromLeft = $forest->where('column', '<', $tree->column)
+                ->where('row', $tree->row)
+                ->max('height') < $tree->height;
+            $visibleFromRight = $forest->where('column', '>', $tree->column)
+                ->where('row', $tree->row)
+                ->max('height') < $tree->height;
+            $visibleFromTop = $forest->where('row', '<', $tree->row)
+                ->where('column', $tree->column)
+                ->max('height') < $tree->height;
+            $visibleFromBottom = $forest->where('row', '>', $tree->row)
+                ->where('column', $tree->column)
+                ->max('height') < $tree->height;
+
+            return $visibleFromLeft || $visibleFromRight || $visibleFromTop || $visibleFromBottom;
+        })->sum();
 
         $question2 = null;
         $answer2 = null;
@@ -20,5 +45,25 @@ class DayEightController extends Controller
             'question2',
             'answer2'
         ));
+    }
+
+    private function getForest(): Collection
+    {
+        $file = Storage::disk('public')->get('day-8.txt');
+
+        return collect(explode("\n", $file))
+            ->filter(fn ($line) => $line !== '')
+            ->map(function ($line, $row) {
+                return collect(str_split($line))
+                    ->map(function ($height, $column) use ($row) {
+                        return (object) [
+                            'row' => $row,
+                            'column' => $column,
+                            'height' => (int) $height,
+                        ];
+                    })
+                    ->toArray();
+            })
+            ->flatten(1);
     }
 }
